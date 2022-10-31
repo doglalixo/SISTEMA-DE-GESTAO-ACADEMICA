6857cf15f0bcadefcf18d16dbdcdb19907d8643e
doglalixo <ddefeio123@gmail.com>
2022-10-31 18:22:21 -0300

    Dando funcionalidade à tela de cadastro

--- sistema-academico.py
+++ sistema-academico.py
@@ -1,13 +1,36 @@
 # IMPORTAÇÃO DE BIBLIOTECAS
+from ast import Try
 from cProfile import label
 import cProfile
+from cgitb import text
 from email import message
 from logging import root
-
 from setuptools import Command
 from bibliotecas import *
-from criardb import *
-#CRIAÇÃO TELA DE CADASTRO
+
+def telaaluno():
+    pass
+    #Função para testar se se o usuario e senha sao validos e se for, realizar o login
+def botaoentrar():
+            try:
+                import sqlite3 as lite
+                con = lite.connect('db.sistema-academico.db')
+                with con:
+                    cur = con.cursor()
+                    cur.execute("SELECT * FROM CADASTRO_USUARIO WHERE USUARIO = " + str(e_usuario.get()))
+                    consulta = cur.fetchall()[0]
+            except IndexError:
+                messagebox.showerror('', 'Usuário ou senha inválido, cheque todos os carácteres.')
+                e_usuario.delete(0, END)
+                e_senha.delete(0, END)
+
+            except lite.OperationalError:
+                messagebox.showerror('', 'Preencha todos os campos.')
+                e_usuario.delete(0, END)
+                e_senha.delete(0, END)
+
+            else:
+                telaaluno()
 def telalogin(): 
     root = Tk() 
     root.title('Tela de login') 
@@ -23,13 +46,12 @@ def telalogin():
 
 
 PARA REALIZAR CADASTRO: Clicar em "Cadastrar-se". """)
-
     def botaocadastrar():
         root.destroy()
         telacadastro()
         pass
     # CRIAÇÃO DO FRAME PARA REALIZAR O LOGIN 
-    frame_login = Frame(root, background='#DCDCDC', highlightthickness=3, highlightbackground='black',relief=GROOVE)
+    frame_login = Frame(root, background='#DCDCDC', highlightthickness=3, highlightbackground='gray',relief=GROOVE)
     frame_login.place(relx=0.33, rely=0.15, relheight=0.60, relwidth=0.40) 
 
     # CRIAÇÃO DOS LABEL's, ENTRYS, BOTÕES E IMAGENS:
@@ -86,12 +108,35 @@ PARA REALIZAR CADASTRO: Clicar em "Cadastrar-se". """)
     e_senha = Entry(frame_login)
     e_senha.place(x=104, y=148, relwidth=0.4)
 
+    #Funções
+
+    #Função para testar se se o usuario e senha sao validos e se for, realizar o login
+    def botaoentrar():
+            try:
+                import sqlite3 as lite
+                con = lite.connect('db.sistema-academico.db')
+                with con:
+                    cur = con.cursor()
+                    cur.execute("SELECT * FROM CADASTRO_USUARIO WHERE USUARIO = " + str(e_usuario.get()))
+                    consulta = cur.fetchall()[0]
+            except IndexError:
+                messagebox.showerror('', 'Usuário ou senha inválido, cheque todos os carácteres.')
+                e_usuario.delete(0, END)
+                e_senha.delete(0, END)
+            except lite.OperationalError:
+                messagebox.showerror('', 'Preencha todos os campos.')
+                e_usuario.delete(0, END)
+                e_senha.delete(0, END)
+
+            else:
+                telaaluno()
+
     #BOTÕES
     borda_ent = LabelFrame(frame_login, bd = 2, bg = "black")
     borda_ent.pack(pady = 10)
     borda_ent.place(relx=0.37, rely=0.51)
     b_login = Button(borda_ent, borderwidth=0.5, text='ENTRAR', font=('Ivy 9 bold'), image=img_entrar, bg='#2E8B57', fg='#000000',
-                    compound=RIGHT, width=80, overrelief=RIDGE, relief=GROOVE, anchor=CENTER)
+                    compound=RIGHT, width=80, overrelief=RIDGE, relief=GROOVE, anchor=CENTER, command=botaoentrar)
     b_login.pack()
 
     borda_cdst = LabelFrame(frame_login, bd = 2, bg = "black")
@@ -119,15 +164,38 @@ def telacadastro():
 
     #Funções
     def botaoajudar():
-        messagebox.showinfo('Ajuda', """Para criar um usuário novo, preencha todos os campos e clique em "Finalizado". 
-        
-Caso já tenha um, clique em "Entrar". """)
+        messagebox.showinfo('', """Para criar um usuário novo, preencha todos os campos e clique em "Finalizado". (Obs: O campo "Usuário" deve ser preenchido apenas por números)
+    
+Caso já tenha um, clique em "Fazer login". """)
+
+    def botaofinalizar():
+                try:
+                    import sqlite3 as lite
+                    con = lite.connect('db.sistema-academico.db')
+                    cur = con.cursor()
+                    cur.execute("INSERT INTO DADOS_CADASTRADOS(USUARIO, NOME, CPF, DATA_NASCIMENTO) VALUES (" + str(e_user.get()) + ',' + " '" + str(e_nome.get()) + "'" + "," + str(e_cpf.get()) + ',' + "'" + str(e_cal.get()) + "'" + ')')
+                except lite.IntegrityError:
+                    messagebox.showerror('', 'Usuário em uso. Tente outro ou faça login, caso seja o seu.')
+                    e_user.delete(0, END)
+                except lite.OperationalError:
+                    messagebox.showerror('', 'Preencha todos os campos corretamente. (O campo usuário deve ser preenchido apenas por números)')
+                else:
+                    if len(e_cpf.get()) > 11 or len(e_cpf.get()) < 11:
+                        messagebox.showerror('', 'Preencha um CPF válido.')
+                        e_cpf.delete(0, END)
+                    else:
+                        id_cadastro = int(e_user.get()) + 100
+                        cur.execute("INSERT INTO CADASTRO_USUARIO(ID_CADASTRO, USUARIO, SENHA) VALUES (" + str(id_cadastro) + ' ,' + e_user.get() + ',' + "'" + e_senha.get() + "'" + ')')
+                        con.commit()
+                        messagebox.showinfo('', 'Conta criada com sucesso!')
+                        cadastro.destroy()
+                        telalogin()
     def botaologin():
         cadastro.destroy()
         telalogin()
     #Criação do frame principal
-    frame_cadastro = Frame(cadastro, background='#DCDCDC', highlightthickness=3, highlightbackground='black',relief=GROOVE)
-    frame_cadastro.place(relx=0.33, rely=0.10, relheight=0.80, relwidth=0.40)
+    frame_cadastro = Frame(cadastro, background='#DCDCDC', highlightthickness=3, highlightbackground='gray',relief=GROOVE)
+    frame_cadastro.place(relx=0.30, rely=0.10, relheight=0.80, relwidth=0.40)
 
     #Imagens
     img_cadastrar = Image.open('icones/cadastro.png')
@@ -142,6 +210,10 @@ Caso já tenha um, clique em "Entrar". """)
     img_cpf = img_cpf.resize ((25,25))
     img_cpf = ImageTk.PhotoImage(img_cpf)
 
+    img_senha = Image.open('icones/chave.png')
+    img_senha = img_senha.resize ((25,20))
+    img_senha = ImageTk.PhotoImage(img_senha)
+
     img_calendario = Image.open('icones/calendario.png')
     img_calendario = img_calendario.resize ((25,25))
     img_calendario = ImageTk.PhotoImage(img_calendario)
@@ -155,7 +227,7 @@ Caso já tenha um, clique em "Entrar". """)
     img_check = ImageTk.PhotoImage(img_check)
 
     img_entrar = Image.open('icones/loginpng.png')
-    img_entrar = img_entrar.resize ((30,30))
+    img_entrar = img_entrar.resize ((20,20))
     img_entrar = ImageTk.PhotoImage(img_entrar)
 
     img_ajuda = Image.open('icones/ajuda.png')
@@ -196,7 +268,12 @@ Caso já tenha um, clique em "Entrar". """)
     l_cal.place(relx=0.15, rely=0.4)
 
     l_txt = Label(frame_cadastro, anchor=CENTER, bg='#DCDCDC', text='Já tem um usuário?', fg='#000000')
-    l_txt.place(relx=0.35, rely=0.79)
+    l_txt.place(relx=0.35, rely=0.86)
+
+    l_senha = Label (frame_cadastro, text="CRIE UMA SENHA: ", height=1, anchor=NW, font=('Ivy 8 bold'), bg='#DCDCDC', fg='#000000' )
+    l_senha.place(relx=0.15, rely=0.6)
+    l_imgsenha = Label(frame_cadastro, bg='#DCDCDC', image=img_senha)
+    l_imgsenha.place(relx=0.07, rely=0.63)
 
     #Entrys
     e_cal = DateEntry (frame_cadastro, width=12, background='#2E8B57', foreground='white', borderwidth=2, year=2022)
@@ -211,19 +288,22 @@ Caso já tenha um, clique em "Entrar". """)
     e_user = Entry (frame_cadastro, width=30)
     e_user.place(relx=0.17, rely=0.54)
 
+    e_senha = Entry (frame_cadastro, width=30)
+    e_senha.place(relx=0.17, rely=0.64)
+
     #Botões
     borda_cdstr = LabelFrame(frame_cadastro, bd = 2, bg = "black")
     borda_cdstr.pack(pady = 10)
-    borda_cdstr.place(relx=0.35, rely=0.65)
-    b_cadastro = Button(borda_cdstr, borderwidth=0.5, text='FINALIZADO!', font=('Ivy 9 bold'), image=img_check, bg='#2E8B57', fg='#000000',command=botaocadastrar,
+    borda_cdstr.place(relx=0.35, rely=0.75)
+    b_cadastro = Button(borda_cdstr, borderwidth=0.5, text='FINALIZADO!', font=('Ivy 9 bold'), image=img_check, bg='#2E8B57', fg='#000000', command=botaofinalizar,
                     compound=RIGHT, width=102, overrelief=RIDGE, relief=GROOVE, anchor=CENTER)
     b_cadastro.pack()
 
     borda_ent = LabelFrame(frame_cadastro, bd = 2, bg = "black")
     borda_ent.pack(pady = 10)
-    borda_ent.place(relx=0.37, rely=0.83)
-    b_login = Button(borda_ent, borderwidth=0.5, text='ENTRAR', font=('Ivy 9 bold'), image=img_entrar, command=botaologin, bg='#2E8B57', fg='#000000',
-                    compound=RIGHT, width=80, overrelief=RIDGE, relief=GROOVE, anchor=CENTER)
+    borda_ent.place(relx=0.34, rely=0.90)
+    b_login = Button(borda_ent, borderwidth=0.5, text='FAZER LOGIN', font=('Ivy 9 bold'), image=img_entrar, command=botaologin, bg='#2E8B57', fg='#000000',
+                    compound=RIGHT, width=110, overrelief=RIDGE, relief=GROOVE, anchor=CENTER, height=12)
     b_login.pack()
 
     b_ajuda = Button(frame_cadastro, borderwidth=0, font=('Ivy 9 bold'), image=img_ajuda, bg='#DCDCDC', fg='#000000', command=botaoajudar,
@@ -244,7 +324,7 @@ style.theme_use("clam")
 
 #Funções
 def botaoajuda():
-    messagebox.showinfo('Ajuda', """PARA REALIZAR LOGIN: Preencha os campos de usuário e senha, em seguida, clique no botão "Entrar" para realizar login.
+    messagebox.showinfo('', """PARA REALIZAR LOGIN: Preencha os campos de usuário e senha, em seguida, clique no botão "Entrar" para realizar login.
 
 
 PARA REALIZAR CADASTRO: Clicar em "Cadastrar-se". """)
@@ -253,7 +333,7 @@ def botaocadastrar():
     root.destroy()
     telacadastro()
  # CRIAÇÃO DO FRAME PARA REALIZAR O LOGIN 
-frame_login = Frame(root, background='#DCDCDC', highlightthickness=3, highlightbackground='black',relief=GROOVE)
+frame_login = Frame(root, background='#DCDCDC', highlightthickness=3, highlightbackground='darkgray',relief=GROOVE)
 frame_login.place(relx=0.33, rely=0.15, relheight=0.60, relwidth=0.40) 
 
 # CRIAÇÃO DOS LABEL's, ENTRYS, BOTÕES E IMAGENS:
@@ -315,7 +395,7 @@ borda_ent = LabelFrame(frame_login, bd = 2, bg = "black")
 borda_ent.pack(pady = 10)
 borda_ent.place(relx=0.37, rely=0.51)
 b_login = Button(borda_ent, borderwidth=0.5, text='ENTRAR', font=('Ivy 9 bold'), image=img_entrar, bg='#2E8B57', fg='#000000',
-                    compound=RIGHT, width=80, overrelief=RIDGE, relief=GROOVE, anchor=CENTER)
+                    compound=RIGHT, width=80, overrelief=RIDGE, relief=GROOVE, anchor=CENTER, command=botaoentrar)
 b_login.pack()
 
 borda_cdst = LabelFrame(frame_login, bd = 2, bg = "black")
