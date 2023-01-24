3aacac1387ae7936fb47f19bafec394263b74a48
doglalixo <ddefeio123@gmail.com>
2023-01-24 18:18:49 -0300

    Importação de imagens

---
+++ bibliotecas.py
@@ -0,0 +1,20 @@
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, A4
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import SimpleDocTemplate, Image
+import webbrowser
+from sqlite3 import adapters
+from tkinter import *
+import tkinter as tk
+from   pickle     import FRAME
+from   statistics import quantiles
+from   tkinter    import*
+from unicodedata import name
+from   tkcalendar import Calendar, DateEntry
+from   tkinter    import Tk, StringVar, ttk
+from   tkinter    import messagebox
+from   tkinter    import filedialog as fd
+from   turtle     import bgcolor, left, width
+from   PIL        import Image, ImageTk
+import tkinter.font as tkFont
